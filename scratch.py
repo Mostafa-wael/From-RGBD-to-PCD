7bbcaa8edf156231662d1ce42a0dcda551a98b36
NadaElmasry <nadaahmedelmasry20@gmail.com>
2021-01-25 01:51:37 +0200

    added numpy calculations

--- scratch.py
+++ scratch.py
@@ -45,11 +45,30 @@ class RGBD2XYZ():
         X = (u - self.cx)*z/self.fx
         return X
 
+    def calcXN(self, u, z):
+         X = (np.multiply((u-self.cx),z))/self.fx
+         return X
+
     def calcY(self, v, z):
         Y = (v - self.cy)*z/self.fy
         return Y
 
+    def calcYN(self,v,z):
+        Y = (np.multiply((v-self.cy),z))/self.fy
+        return Y
+
     def xyz_array(self, depthImage):
+        global ID
+        print(ID)
+        if ID == 0:
+            f3 = open("depthimage.txt", "w")
+            for row in depthImage:
+                np.savetxt(f3, row)
+            f3.close()      
+            print("depthImage: ")
+            print(depthImage)
+            print("depthImage Shape: ")
+            print(depthImage.shape)          
         pcd = PointCloud()
         pcd.header.frame_id = "front_left_bumblebee_body"
         temp = Point32()
@@ -57,15 +76,56 @@ class RGBD2XYZ():
         temp.y = 0
         temp.x = 0
         pcd.points = []
+        rows = np.zeros((480,640),dtype=float)
+        rows[:,:] = np.arange(0,640)
+        cols = np.zeros((480,640),dtype=float)
+        cols[:,:] = np.array([np.arange(0,480)]).T
+        z = self.calcZ(depthImage)
+        y = self.calcYN(cols,z)
+        x = self.calcXN(rows,z)
+        if ID==0:
+            ff = open("numpyresult.txt", "w")
         for i in range(0, self.h):
             for j in range(0, self.w):
-                tempPoint = Point32()
-                tempPoint.z = self.calcZ(depthImage[i][j])
-                tempPoint.y = self.calcY(i, tempPoint.z)
-                tempPoint.x = self.calcX(j, tempPoint.z)
-                pcd.points.append(tempPoint)
+                tempPointt = Point32()
+                tempPointt.x , tempPointt.y , tempPointt.z = x[i][j] , y[i][j] , z[i][j]
+                if ID == 0:
+                    ff.write(str("X: "+str(tempPointt.x) + ", Y: " + str(
+                        tempPointt.y) + ", Z: " + str(tempPointt.z) + "\n"))
+                pcd.points.append(tempPointt)
+        if ID==0:
+            ff.close()
+
+        if ID==0:
+            print("z matrix : " )
+            print(z)
+            print("z matrix shape : ")
+            print(z.shape)
+            print("y matrix : ")
+            print(y)
+            print("y matrix shape : ")
+            print(y.shape)
+            print("x matrix : ")
+            print(x)
+            print("x matrix shape : ")
+            print(x.shape)
+#        if ID==0:
+#            f = open("loopresult.txt",'w')
+#        for i in range(0, self.h):
+#            for j in range(0, self.w):
+#                tempPoint = Point32()
+#                tempPoint.z = self.calcZ(depthImage[i][j])
+#                tempPoint.y = self.calcY(i, tempPoint.z)
+#                tempPoint.x = self.calcX(j, tempPoint.z)
+#                pcd.points.append(tempPoint)
+#                if ID == 0:
+#                    f.write(str("X: "+str(pcd.points[i+j].x) + ", Y: " + str(
+#                        pcd.points[i+j].y) + ", Z: " + str(pcd.points[i+j].z) + "\n"))
+#        if ID==0:
+#            f.close()
         ######################
         self.XYZ_pub.publish(pcd)
+        ID+=1
         return pcd.points
 
 
@@ -86,6 +146,7 @@ if __name__ == "__main__":
     try:
         rospy.init_node("camera_info")
         obj = RGBD2XYZ()
+        ID=0
         imageRec = False
         depth_sub = rospy.Subscriber(
             '/airsim_node/PhysXCar/front_left_bumblebee/DepthPlanner', Image, callback_depthImage, queue_size=1)
