31084e1f7661093d12d2f7cca886f2b0580ddd23
Mostafa <mostafa.w.k000@gmail.com>
2021-01-27 22:09:37 +0200

    listener not working

--- workSpace/src/map.py
+++ workSpace/src/map.py
@@ -10,14 +10,28 @@ from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 
 
 def callback_printPCD(PCD):
-    print(PCD.header)
+    global pcd_in
+    pcd_in = PCD
+    # print(pcd_in.header)
+    # cloud_out = do_transform_cloud(PCD, transform)
+
+    listener.waitForTransform("/world_ned", "/front_left_bumblebee_body", rospy.Time(0),rospy.Duration(4.0))
+    pcd_out = listener.transformPointCloud("/world_ned", pcd_in)
+    print(pcd_out)
+
 
 if __name__ == "__main__":
 
     try:
+        ###
         rospy.init_node("ANM", anonymous=True)
-        depth_sub = rospy.Subscriber(
+        pcd_in = PointCloud()
+        pcd_out = PointCloud()
+        listener = tf.TransformListener()
+        PCD_sub = rospy.Subscriber(
             '/PointCloud_ANM', PointCloud, callback_printPCD, queue_size=1)  # queue size of length 1 i.e. hold only one object and process it
+        ####
+
         rospy.spin()
 
     except rospy.ROSInterruptException:
